c5d6e70e95619753c43ef2ff5e2ee9ba155fb810
MichelleDuranTEC <157264977+MichelleDuranTEC@users.noreply.github.com>
2025-06-08 20:00:54 -0600

    Update firebase-config.js
    Edición de texto

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,3 @@
-// Reemplaza con tu configuración
 const firebaseConfig = {
   apiKey: "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",
   authDomain: "DOMINIO.firebaseapp.com",
