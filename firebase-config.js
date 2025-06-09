75abadeef3696fb374c7f8844ff864a8c9433eef
MichelleDuranTEC <157264977+MichelleDuranTEC@users.noreply.github.com>
2025-06-08 19:13:20 -0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
 // Reemplaza con tu configuración
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_DOMINIO.firebaseapp.com",
-  projectId: "TU_ID",
-  storageBucket: "TU_BUCKET",
-  messagingSenderId: "TU_SENDER_ID",
-  appId: "TU_APP_ID"
+  apiKey: "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",
+  authDomain: "DOMINIO.firebaseapp.com",
+  projectId: "ID",
+  storageBucket: "BUCKET",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID"
 };
 
 firebase.initializeApp(firebaseConfig);
